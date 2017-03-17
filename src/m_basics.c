58062e85d5c514279bc1cb2aff8c306660c60427
Evandro Murilo <evandromurilob@hotmail.com>
2017-03-17 09:51:53 -0400

    renamed read to readn and included some new functions

---
+++ src/m_basics.c
@@ -0,0 +1,168 @@
+#include "m_basics.h"
+
+char *concat(char *a, char *b) {
+	int len_a = len(a);
+	int len_b = len(b);
+	char *c = malloc(len_a + len_b + 1);
+
+	if (c == NULL) {
+		printf("ERROR: malloc failed in concat\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (*a != '\0') *(c++) = *(a++);
+	while (*b != '\0') *(c++) = *(b++);
+	*c = '\0';
+
+	return c-len_a-len_b;
+}
+
+int len(char *s) {
+	char *a = s;
+	while (*a != '\0') ++a;
+
+	return a-s;
+}
+
+int strcmpr_first(char *a, char *b, int n) {
+	for (int i = 0; i < n; ++i) 
+		if (a[i] != b[i]) return a[i] - b[i];
+
+	return 0;
+}
+
+int pushed = EOF-1;
+
+int read_char() {
+	char c;
+
+	if (pushed >= EOF) {
+		c = pushed;
+		pushed = EOF-1;
+	}
+	else {
+		c = getchar();
+	}
+
+	return c;
+}
+
+int push_char(int c) {
+	pushed = c;
+	return c;
+}
+
+int readn(char *arr, int n) {
+	int c, i = 0;
+	while (i < n && (c = read_char()) != EOF) arr[i++] = c;
+
+	arr[i] = '\0';
+	return i;
+}
+
+int read_line(char *arr, int size) {
+	int c, i = 0;
+	while (i < size-1 && (c = read_char()) != EOF && c != '\n') arr[i++] = c;
+
+	arr[i] = '\0';
+	return i;
+}
+
+int discard_line() {
+	int c, n = 0;
+	while ((c = read_char()) != '\n' && c != EOF) ++n;
+
+	return n;
+}
+
+int discard_until(int goal) {
+	int c, n = 0;
+	while ((c = read_char()) != goal && c != EOF) ++n;
+
+	push_char(c);
+	return n;
+}
+
+int discard_while(char *goals) {
+	int c, n = 0;
+	bool found = true;
+
+	while (found && (c = read_char()) != EOF) {
+		found = false;
+		for (int i = 0; goals[i] != '\0'; ++i) {
+			if (goals[i] == c) {
+				found = true;
+				++n;
+				break;
+			}
+		}
+	}
+
+	push_char(c);
+	return n;
+}
+
+int read_until(int goal, char* str, int size) {
+	int c, i;
+
+	for (i = 0; i < size-1 && (c = read_char()) != goal && c != EOF; ++i, ++str) {
+		*str = c;
+	}
+
+	*str = '\0';
+	return i;
+}
+
+#define MAX_WORD_SIZE 100
+int split(char** arr, int size, char* str, char goal) {
+	char word[MAX_WORD_SIZE+1];
+	int i, c, total = 0;
+
+	while (total < size && *str != '\0') {
+		while (*str == goal && *str != '\0') ++str;
+
+		if (*str == '\0') break;
+
+		for (i = 0; i < MAX_WORD_SIZE && *str != goal && *str != '\0'; 
+				++i, ++str) {
+			word[i] = *str;
+		}
+		word[i] = '\0';
+
+		arr[total] = malloc(i);
+		strcpy(arr[total], word);
+		printf("Read word: %s\n", arr[total]);
+		++total;
+	}
+
+	return total;
+}
+
+bool is_prime(int n) {
+	if (n == 2) return true;
+	if (n == 3) return true;
+	if (n % 2 == 0) return false;
+	if (n % 3 == 0) return false;
+
+	int i = 5;
+	int w = 2;
+
+	while (i*i <= n) { // would it be better to calculate sqrt of n?
+		if (n % i == 0) return false;
+		i += w;
+		w = 6-w; // alterns between 2 and 4, skipping multiples of 2 and 3
+	}
+	return true;
+}
+
+int next_prime(int n) {
+	if (n%2==0) ++n;
+	else n+=2;
+
+	int w = 2;
+	while (true) {
+		if (is_prime(n)) return n;
+		n += w;
+		w = 6-w;
+	}
+}
---
+++ src/m_basics.h
@@ -0,0 +1,65 @@
+#ifndef M_BASICS_H
+#define M_BASICS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// len: returns the length of the string 's'
+int len(char *s);
+
+// concat: concatenates 'a' and 'b', returns a new string
+char *concat(char *a, char *b);
+
+// readn: reads up to 'n' chars into the string 'arr'
+// returns the amount of chars read
+int readn(char *arr, int n);
+
+// read_line: reads in the string 'arr' until '\n' or 'EOF' is found
+// or 'size'-1 chars are read
+// returns the amount of chars read
+int read_line(char *arr, int size);
+
+// strcmpr_first: compares the first 'n' chars of the strings 'a' and 'b';
+// if a > b,  returns > 0
+// if b > a,  returns < 0
+// if a == b, returns 0
+int strcmpr_first(char *a, char *b, int n);
+
+// read_char: returns the pushed char or a char read with getchar()
+int read_char();
+
+// push_char: pushes a char
+// returns the pushed char
+int push_char(int c);
+
+// discard_line: discards a line
+// returns the amount of chars discarded
+int discard_line();
+
+// discard_until: discards chars until 'goal' or EOF is found
+// returns the amount of chars discarded
+int discard_until(int goal);
+
+// discard_while: discards chars while they are present in the string 'goals'
+// returns the amount of chars discarded
+int discard_while(char *goals);
+
+// read_until: reads chars into 'str' until 'goal' or EOF is found, or 'size'-1 
+// chars are read
+// returns the amount of chars read
+int read_until(int goal, char* str, int size);
+
+// split: splits 'str' into various strings with 'goal' being the separator
+// strings are stored in the array 'arr' of size 'size'
+// returns the resulting amount of strings
+int split(char** arr, int size, char* str, char goal);
+
+// is_prime: returns true if n is prime
+bool is_prime(int n);
+
+// next_prime: returns the next prime closest to n
+int next_prime(int n);
+
+#endif
